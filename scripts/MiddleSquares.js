eb7c435d6feb0a81bc0929acba0ca75694423146
felipGonzalez <afelipgb@gmail.com>
2020-06-27 08:26:13 -0500

    cm

--- scripts/MiddleSquares.js
+++ scripts/MiddleSquares.js
@@ -118,19 +118,6 @@ window.addEventListener('load', function () {
     }
 
 
-    /*
-    var tableRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
-
-// Insert a row in the table at the last row
-var newRow   = tableRef.insertRow();
-
-// Insert a cell in the row at index 0
-var newCell  = newRow.insertCell(0);
-
-// Append a text node to the cell
-var newText  = document.createTextNode('New row');
-newCell.appendChild(newText);
-    */
 
 });
 
