5b8e2fafb6a3accd5ff9b732309bcc64b91379e0
daniel hizo un comentario <daniellopezj0327@gmail.com>
2020-07-04 14:08:12 -0500

    changes

--- scripts/MiddleSquares.js
+++ scripts/MiddleSquares.js
@@ -1,105 +1,105 @@
-window.addEventListener('load', function () {
+window.addEventListener('load', function() {
 
     /** Cuadrados medios */
     class MiddleSquares {
-        
+
         min = 5;
         max = 10;
         k = 2;
         divisor = 0
 
-        init(seed, quantity,a,b){
-            if(a != undefined && a > 0){
+        init(seed, quantity, a, b) {
+            if (a != undefined && a > 0) {
                 this.min = a
             }
-            if(b != undefined && b > 0){
+            if (b != undefined && b > 0) {
                 this.min = b
             }
             this.divisor = this.getDivisor(seed.toString().length);
-            return this.createMatrix(parseInt(seed),parseInt(quantity));
+            return this.createMatrix(parseInt(seed), parseInt(quantity));
         }
-        
-        createMatrix(seed, quantity){
+
+        createMatrix(seed, quantity) {
             let result = [];
             var xi = seed
             for (let i = 0; i < quantity; i++) {
-                let xi2 = Math.pow(xi, 2);    
+                let xi2 = Math.pow(xi, 2);
                 let extension = xi2.toString().length
                 let extraction = parseInt(this.verifyNumber(xi2))
-                let ri = extraction/this.divisor
+                let ri = extraction / this.divisor
                 let ni = this.calculateNi(ri)
                 let data = []
-                data.push(i,xi,xi2,extension,extraction,ri,ni)
+                data.push(i, xi, xi2, extension, extraction, ri, ni)
                 result.push(data);
                 xi = extraction
             }
             return result;
         }
 
-        verifyNumber(number){
-            if( number.toString().length >= this.min & number.toString().length <= this.max){
+        verifyNumber(number) {
+            if (number.toString().length >= this.min & number.toString().length <= this.max) {
                 //console.log("El tamaño esta bien  ", number);
-                
-                if(number.toString().length % 2 == 0){
+
+                if (number.toString().length % 2 == 0) {
                     //console.log("Es par  " , number);
-                    return this.extractNumber(number+"");
-                }else{
+                    return this.extractNumber(number + "");
+                } else {
                     //console.log("Es impar  " , number);
-                    return this.extractNumber(this.addZero(number,1));
+                    return this.extractNumber(this.addZero(number, 1));
                 }
-            }else if (number.toString().length < this.min) {
+            } else if (number.toString().length < this.min) {
                 //console.log("Es menor, toca agregar ceros ", number);
                 let numberLength = number.toString().length;
                 let count = this.min - number.toString().length;
-                return this.extractNumber(this.addZero(number,count));
-            } 
-        } 
-        
-        addZero(number, count){
-            let newNumber = ""
-            for (let i = 0; i < count ; i++) {
-                newNumber+="0"
+                return this.extractNumber(this.addZero(number, count));
             }
-            newNumber+=number
-            return newNumber
-       }
-
-       extractNumber(numberString){
-        //console.log(typeof(numberString));
-        let size = numberString.length/2
-        let extract = numberString.substr(size-this.k,this.k*2)
-        return extract;
-       }
-
-       getDivisor(count){
-            let divisor = 1;
-        for (let i = 0; i < count; i++) {
-            divisor *= 10;            
         }
-        console.log("divisor",divisor);
-        
-        return divisor;
-       }
 
-       calculateNi(ri){
-        let ni = this.min + (this.max - this.min)*ri
-        return ni;
-       }
+        addZero(number, count) {
+            let newNumber = ""
+            for (let i = 0; i < count; i++) {
+                newNumber += "0"
+            }
+            newNumber += number
+            return newNumber
+        }
+
+        extractNumber(numberString) {
+            //console.log(typeof(numberString));
+            let size = numberString.length / 2
+            let extract = numberString.substr(size - this.k, this.k * 2)
+            return extract;
+        }
+
+        getDivisor(count) {
+            let divisor = 1;
+            for (let i = 0; i < count; i++) {
+                divisor *= 10;
+            }
+            console.log("divisor", divisor);
+
+            return divisor;
+        }
+
+        calculateNi(ri) {
+            let ni = this.min + (this.max - this.min) * ri
+            return ni;
+        }
 
     }
 
-    
 
-    document.querySelector("#btn-c").addEventListener('click', function () {
+
+    document.querySelector("#btn-c").addEventListener('click', function() {
         middleSquares = new MiddleSquares();
 
-       let data = middleSquares.init(
-           document.querySelector("#seed").value,
-           document.querySelector("#count").value,
-           document.querySelector("#a").value,
-           document.querySelector("#b").value
-           );
-       
+        let data = middleSquares.init(
+            document.querySelector("#seed").value,
+            document.querySelector("#count").value,
+            document.querySelector("#a").value,
+            document.querySelector("#b").value
+        );
+
         localStorage.setItem("ri", JSON.stringify(data[5]));
         let ri = []
         for (let i = 0; i < data.length; i++) {
@@ -108,9 +108,9 @@ window.addEventListener('load', function () {
         sessionStorage.clear()
         sessionStorage.setItem("ri", JSON.stringify(ri))
         var table = document.querySelector("#table-c");
-        createTable(table,data);
+        createTable(table, data);
     });
-    
+
 
     function createTable(table, data) {
         var body = document.createElement("tbody");
@@ -124,14 +124,9 @@ window.addEventListener('load', function () {
             }
             body.appendChild(tr);
         }
-        
+
         var new_tbody = document.createElement('tbody');
         table.replaceChild(body, document.querySelector("#table-c>tbody"))
-         
+
     }
-
-
-
 });
-
-
